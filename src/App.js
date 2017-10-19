bfc4eb84cf74a51e0595daf29481b76d2973c5c6
badboy99tw <taiwan.badboy99@gmail.com>
2017-10-19 20:57:50 +0800

    Introduce TabNavigator with screen Warehouse and Setting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import { TabNavigator } from 'react-navigation'
 
 import { FarmScreen } from './screens/FarmScreen'
+import { SettingScreen } from './screens/SettingScreen'
+import { WarehouseScreen } from './screens/WarehouseScreen'
 
 export default class App extends React.Component {
   render() {
-    return (
-      <FarmScreen />
-    )
+    return <MainNavigator/>
   }
 }
+
+const MainNavigator = TabNavigator({
+  Farm: {
+    screen: FarmScreen,
+    navigationOptions: {
+      title: 'Farm',
+    },
+  },
+  Warehouse: {
+    screen: WarehouseScreen,
+    navigationOptions: {
+      title: 'Warehouse',
+    },
+  },
+  Setting: {
+    screen: SettingScreen,
+    navigationOptions: {
+      title: 'Setting',
+    },
+  },
+})
---
+++ src/screens/WarehouseScreen.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Button, StyleSheet, View } from 'react-native'
+
+export class WarehouseScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title='Tools'/>
+        <Button title='Machines'/>
+        <Button title='Seeds'/>
+        <Button title='Pesticides'/>
+        <Button title='Fertilizers'/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
