aaa777522c33f179e65962f92d971258a50e45c1
badboy99tw <taiwan.badboy99@gmail.com>
2017-10-21 04:26:41 +0800

    Refactor, remove unused import

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationActions } from 'react-navigation'
 
 import { FacebookLoginButton } from '../components/FacebookLoginButton'
