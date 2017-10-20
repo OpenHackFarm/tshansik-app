df113772347e4c1b2ded12716f046dba54d1835b
badboy99tw <taiwan.badboy99@gmail.com>
2017-10-21 04:42:16 +0800

    Add ProfileScreen

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { Button, StyleSheet, View } from 'react-native'
+import { StackNavigator } from 'react-navigation'
 
-export default class SettingScreen extends React.Component {
+import ProfileScreen from './ProfileScreen'
+
+class SettingScreen extends React.Component {
   render() {
+    const { navigate } = this.props.navigation
     return (
       <View style={styles.container}>
-        <Button title='Profile'/>
+        <Button
+          title='Profile'
+          onPress={() =>{
+            navigate('Profile')}}/>
         <Button title='Terms of Service'/>
         <Button title='Report a Problem'/>
       </View>
     )
   }
 }
+
+export default StackNavigator({
+  Menu: { screen: SettingScreen },
+  Profile: { screen: ProfileScreen },
+})
 
 const styles = StyleSheet.create({
   container: {
