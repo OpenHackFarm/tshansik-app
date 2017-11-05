4197cf88768fb46e3aef27489019d00d9472ac78
badboy99tw <taiwan.badboy99@gmail.com>
2017-11-06 00:04:24 +0800

    Introduce native-base, load font on app init stage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AppLoading } from 'expo'
 import { Provider } from 'mobx-react'
 import { observer } from 'mobx-react'
 
@@ -13,7 +14,27 @@ const stores = {
 
 @observer
 export default class App extends React.Component {
+  state = {
+    isReady: false,
+  }
+
+  init = async () => {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    })
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this.init}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      )
+    }
     const Layout = createRootNavigator(authStore.signedIn)
     return (
       <Provider {...stores}>
